ad47dabe147829b15f4e27a45bf6dfc513aa2932
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-10-06 10:14:05 +0430

    Fixed the linters errors

--- public/script.js
+++ public/script.js
@@ -118,10 +118,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   searchBar.addEventListener('input', () => {
     const query = searchBar.value.toLowerCase();
-    const filteredBooks = books.filter((book) =>
-      book.title.toLowerCase().includes(query) ||
-      book.author.toLowerCase().includes(query)
-    );
+    const filteredBooks = books.filter((book) => book.title.toLowerCase().includes(query)
+    ||
+      book.author.toLowerCase().includes(query));
     displayBooks(filteredBooks);
   });
 
